c7c4573d03c4a039c47a0253bd98bb153108de35
Dexter McMillan <dexmcmillan@gmail.com>
2023-04-06 10:07:01 -0400

    update to url used

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import pandas as pd
 import geopandas
 from datetime import datetime
 
-raw = geopandas.read_file("https://services5.arcgis.com/0akaykIdiPuMhFIy/arcgis/rest/services/bs_infoPannes_prod_vue/FeatureServer/0/query?where=1=1&outFields=*&geometryType=esriGeometryEnvelope&spatialRel=esriSpatialRelIntersects&resultType=tile&f=geojson")
+raw = geopandas.read_file("https://services5.arcgis.com/0akaykIdiPuMhFIy/arcgis/rest/services/bs_infoPannes_prod_vue/FeatureServer/1/query?returnGeometry=true&where=1=1&outSR=4326&outFields=*&inSr=4326&geometryType=esriGeometryEnvelope&spatialRel=esriSpatialRelIntersects&geometryPrecision=6&resultType=tile&f=geojson")
 raw = raw.set_index('OBJECTID')
 
 now = datetime.now()
